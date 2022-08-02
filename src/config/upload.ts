14be4d43db9287169d10cdd372c267140e92aaa0
FelipeBigarelli <felipebigarelli@hotmail.com>
2022-08-02 02:40:10 -0300

    Upload validations

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -6,7 +6,6 @@ const tmpFolder = resolve(__dirname, '..', '..', 'tmp');
 
 export default {
   tmpFolder,
-
   storage: multer.diskStorage({
     destination: tmpFolder,
     filename: (request, file, callback) => {
@@ -16,4 +15,20 @@ export default {
       return callback(null, fileName);
     },
   }),
+  limits: { fileSize: 300000 },
+  fileFilter: (request: any, file: { mimetype: string }, callback: any) => {
+    if (
+      file.mimetype === 'image/png' ||
+      file.mimetype === 'image/jpg' ||
+      file.mimetype === 'image/jpeg'
+    ) {
+      callback(null, true);
+
+      return true;
+    }
+    callback(null, false);
+    return callback(
+      new Error('Somente formato .png, .jpg e .jpeg são permitidos')
+    );
+  },
 };
